9a291c58471fd3db3fe354d8aeaff8cb27b4a035
zxc <18811537110@126.com>
2017-09-06 15:07:02 +0800

    commit

--- app/src/main/java/com/example/administrator/android_practice/MainActivity.java
+++ app/src/main/java/com/example/administrator/android_practice/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("111", "comit");
         setContentView(R.layout.activity_main);
     }
 
